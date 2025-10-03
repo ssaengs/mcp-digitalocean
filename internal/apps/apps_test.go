d135a4f7b05034afab1a229145c4e6fca5e6a37d
Soulan Saeng <ssaeng@digitalocean.com>
2025-10-03 12:44:33 -0400

    remove tests.

--- internal/apps/apps_test.go
+++ internal/apps/apps_test.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/digitalocean/godo"
 	"github.com/mark3labs/mcp-go/mcp"
@@ -526,16 +525,4 @@ func TestGetDeploymentStatus(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestAnotherLongRunningOperation(t *testing.T) {
-	t.Parallel()
-	time.Sleep(30 * time.Second)
-	require.True(t, true)
-}
-
-func TestLongRunningOperation(t *testing.T) {
-	t.Parallel()
-	time.Sleep(30 * time.Second)
-	require.True(t, true)
 }
