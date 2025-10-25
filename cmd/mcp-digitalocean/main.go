040465dfe592f10510cc724ceda8c98ecd193387
Soulan Saeng <ssaeng@digitalocean.com>
2025-10-25 16:18:12 -0400

    release v1.0.15

--- cmd/mcp-digitalocean/main.go
+++ cmd/mcp-digitalocean/main.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	mcpName    = "mcp-digitalocean"
-	mcpVersion = "1.0.14"
+	mcpVersion = "1.0.15"
 )
 
 // getEnv retrieves the value of the environment variable named by the key.
