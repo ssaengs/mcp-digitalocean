c32f37e0f5b9c10c29ea358ecd74efc95d582fe4
Soulan Saeng <ssaeng@digitalocean.com>
2025-11-26 09:18:05 -0500

    adding droplet timeouts

--- cmd/mcp-digitalocean/main.go
+++ cmd/mcp-digitalocean/main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	mcpName                 = "mcp-digitalocean"
-	mcpVersion              = "1.0.25"
+	mcpVersion              = "1.0.26"
 	wsLoggingContextTimeout = 15 * time.Second
 )
 
