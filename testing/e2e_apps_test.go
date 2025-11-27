4399ed4ea3ef1002eafe3032f29f986c1306257d
Soulan Saeng <ssaeng@digitalocean.com>
2025-11-27 11:14:28 -0500

    check the deployment status for apps e2e

--- testing/e2e_apps_test.go
+++ testing/e2e_apps_test.go
@@ -5,11 +5,15 @@ package testing
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/digitalocean/godo"
+	"github.com/google/uuid"
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/stretchr/testify/require"
+
+	"mcp-digitalocean/pkg/registry/apps"
 )
 
 func TestCreateApp(t *testing.T) {
@@ -17,9 +21,11 @@ func TestCreateApp(t *testing.T) {
 	c := initializeClient(ctx, t)
 	defer c.Close()
 
+	// Generate a short unique ID for the app name
+	shortID := fmt.Sprintf("%08x", uuid.New().ID())
 	create := godo.AppCreateRequest{
 		Spec: &godo.AppSpec{
-			Name: "mcp-e2e-test",
+			Name: fmt.Sprintf("mcp-%s", shortID),
 			Services: []*godo.AppServiceSpec{
 				{
 					Name: "sample-golang",
@@ -49,7 +55,27 @@ func TestCreateApp(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, app.ID)
 
-	t.Logf("app ID: %s\n", app.ID)
+	t.Logf("Created app: %s\n", app.Spec.Name)
+
+	// Get the app deployment status
+	status, err := c.CallTool(context.Background(), mcp.CallToolRequest{
+		Params: mcp.CallToolParams{
+			Name: "apps-get-deployment-status",
+			Arguments: map[string]interface{}{
+				"AppID": app.ID,
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.NotNil(t, status)
+	require.False(t, status.IsError)
+
+	// unmarshall the response to an app deployment
+	var deployment apps.DeploymentStatus
+	deploymentJSON := status.Content[0].(mcp.TextContent).Text
+	err = json.Unmarshal([]byte(deploymentJSON), &deployment)
+	require.NoError(t, err)
+	t.Logf("App deployment phase: %s", deployment.Deployment.Phase)
 
 	// cleanup the app
 	resp, err = c.CallTool(ctx, mcp.CallToolRequest{
