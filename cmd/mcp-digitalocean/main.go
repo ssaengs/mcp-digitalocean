f4aa9a80b66a01ecba3739b142c3a29a865f8827
Soulan Saeng <ssaeng@digitalocean.com>
2025-12-02 08:32:12 -0500

    increase version for release

--- cmd/mcp-digitalocean/main.go
+++ cmd/mcp-digitalocean/main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	mcpName                 = "mcp-digitalocean"
-	mcpVersion              = "1.0.27"
+	mcpVersion              = "1.0.28"
 	wsLoggingContextTimeout = 15 * time.Second
 )
 
