837928ee68272018ec307e0ad22a6ad725aecf7e
Soulan Saeng <ssaeng@digitalocean.com>
2026-03-10 11:53:55 -0400

    bump version

--- cmd/mcp-digitalocean/main.go
+++ cmd/mcp-digitalocean/main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	mcpName                 = "mcp-digitalocean"
-	mcpVersion              = "1.0.31"
+	mcpVersion              = "1.0.32"
 	wsLoggingContextTimeout = 15 * time.Second
 )
 
